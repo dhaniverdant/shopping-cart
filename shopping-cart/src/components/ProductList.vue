3395be844e747a490cdcbd5495680fb29e01c4fe
dhani <dhaniverdant23@gmail.com>
2021-01-28 14:09:26 +0700

    add products to cart and use dev tools to see mutation history

--- shopping-cart/src/components/ProductList.vue
+++ shopping-cart/src/components/ProductList.vue
@@ -1,38 +1,41 @@
 <template>
-  <div>
-    <h1>Product List</h1>
-    <img
-      v-if="loading"
-      src="https://i.imgur.com/JfPpwOA.gif"
-    >
-    <ul v-else>
-      <li v-for="product in products" :key="product.name">{{product.title}} - {{product.price}}</li>
-    </ul>
-  </div>
+    <div>
+      <h1>Product List</h1>
+      <img
+        v-if="loading"
+        src="https://i.imgur.com/JfPpwOA.gif"
+      >
+      <ul v-else>
+        <li v-for="product in products">
+          {{product.title}} - {{product.price}} - {{product.inventory}}
+          <button @click="addProductToCart(product)">Add to cart</button>
+        </li>
+      </ul>
+    </div>
 </template>
-
 <script>
-
-export default {
-  data () {
-    return {
-      loading: false
-    }
-  },
-
-  computed: {
-    products() {
-      return this.$store.getters.availableProducts;
+  export default {
+    data () {
+      return {
+        loading: false
+      }
     },
-  },
-
-  created() {
-    this.loading = true
+    computed: {
+      products () {
+        return this.$store.getters.availableProducts
+      }
+    },
+    methods: {
+      addProductToCart (product) {
+        this.$store.dispatch('addProductToCart', product)
+      }
+    },
+    created () {
+      this.loading = true
       this.$store.dispatch('fetchProducts')
         .then(() => this.loading = false)
-  },
-};
+    }
+  }
 </script>
-
-<style>
+<style scoped>
 </style>
--- shopping-cart/src/store/index.js
+++ shopping-cart/src/store/index.js
@@ -1,12 +1,13 @@
-import Vuex from 'vuex';
-import Vue from 'vue';
-
-import shop from '@/api/shop';
-Vue.use(Vuex);
+import Vuex from 'vuex'
+import Vue from 'vue'
+import shop from '@/api/shop'
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { // = data
     products: [],
+    // {id, quantity}
+    cart: []
   },
 
   getters: { // = computed properties
@@ -14,7 +15,6 @@ export default new Vuex.Store({
       return state.products.filter(product => product.inventory > 0)
     }
   },
-
   actions: { // = methods
     fetchProducts ({commit}) {
       return new Promise((resolve, reject) => {
@@ -25,13 +25,40 @@ export default new Vuex.Store({
           resolve()
         })
       })
+    },
+
+    addProductToCart (context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id)
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id)
+        } else {
+          context.commit('incrementItemQuantity', cartItem)
+        }
+        context.commit('decrementProductInventory', product)
+      }
     }
   },
 
   mutations: {
-    setProducts(state, products) {
+    setProducts (state, products) {
       // update products
-      state.products = products;
+      state.products = products
     },
-  },
-});
+
+    pushProductToCart (state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+
+    incrementItemQuantity (state, cartItem) {
+      cartItem.quantity++
+    },
+
+    decrementProductInventory (state, product) {
+      product.inventory--
+    }
+  }
+})
