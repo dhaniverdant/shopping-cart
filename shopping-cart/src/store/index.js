d03271f5dfb34efaaf3cf7dfdf3c8dc7580338a2
dhani <dhaniverdant23@gmail.com>
2021-01-28 11:17:30 +0700

    install vuex and use

---
+++ shopping-cart/src/store/index.js
@@ -0,0 +1,29 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: { // = data
+    products: [],
+  },
+
+  getters: { // = computed properties
+    productsCount() {
+      // ...
+    },
+  },
+
+  actions: {
+    fetchProducts() {
+      // make the call
+    },
+  },
+
+  mutations: {
+    setProducts(state, products) {
+      // update products
+      state.products;
+    },
+  },
+});
